5b62afd1b0e410fab23dd04e1be97c085c4f8e8e
iStudent <ja.xi.es@hotmail.com>
2016-10-31 16:10:16 -0400

    test commit

--- website-project-1/js/ubet-home.js
+++ website-project-1/js/ubet-home.js
@@ -7,15 +7,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	 for (var i = 0; i < num_images; i ++) {
 
-	 	imageHover[i].addEventListener("onmouseover", clicked);
-
-	 }
+	 	eventListener(imageHover[i], "mouseover", mouseOverCallback);
+	 } 
 
 	 // show image-bottom when hover over image-box
-	function clicked(e) {
 
-		var bottom = e.target.parentNode.getElementsByClassName("image-bottom")[0];
-		console.log(bottom);
-		bottom.style.visibility = "visible";
-	}
 });
+
+function mouseOverCallback(e) {
+
+
+	var image_box = e.target.parentNode;
+	var bottom = image_box.getElementsByClassName("image-bottom")[0];
+	bottom.style.visibility = "visible";
+	eventListener(image_box, "mouseleave", mouseOutCallback)
+}
+
+function eventListener(element, event, callback) {
+
+	element.addEventListener(event, callback);
+}
+
+function mouseOutCallback(e) {
+	
+	var image_box = e.target.parentNode;
+	var bottom = image_box.getElementsByClassName("image-bottom")[0];
+	bottom.style.visibility = "hidden";
+}
