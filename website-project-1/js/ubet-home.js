6876754d606d971c62f41ad83143b4023345f970
iStudent <ja.xi.es@hotmail.com>
2016-11-01 16:00:19 -0400

    added featured section

--- website-project-1/js/ubet-home.js
+++ website-project-1/js/ubet-home.js
@@ -1,36 +1,40 @@
+/*
+	Wait until the page finishes loading, and then start action
+*/
+
 document.addEventListener("DOMContentLoaded", function(event) { 
   //do work
 
-	var imageHover = document.getElementsByClassName("event-box");
+	var imageHover = document.getElementById('event-div').getElementsByClassName("event-box");
 
-	var num_images = imageHover.length;
+	var num_images = imageHover.length - 1; // remove the last element because it does not have an img
 
-	 for (var i = 0; i < num_images; i ++) {
+	for (var i = 0; i < num_images; i ++) {
 
-	 	eventListener(imageHover[i], "mouseover", mouseOverCallback);
-	 } 
+		eventListener(imageHover[i], "mouseover", mouseOverCallback);
+		eventListener(imageHover[i], "mouseleave", mouseOutCallback);
+	} 
 
-	 // show image-bottom when hover over image-box
+	// show image-bottom when hover over image-box
 
 });
 
-function mouseOverCallback(e) {
+function mouseOverCallback(target) {
 
-
-	var image_box = e.target.parentNode;
-	var bottom = image_box.getElementsByClassName("image-bottom")[0];
+	var bottom = target.getElementsByClassName("image-bottom")[0];
 	bottom.style.visibility = "visible";
-	eventListener(image_box, "mouseleave", mouseOutCallback)
+}
+
+
+function mouseOutCallback(target) {
+	
+	var bottom = target.getElementsByClassName("image-bottom")[0];
+	bottom.style.visibility = "hidden";
 }
 
 function eventListener(element, event, callback) {
 
-	element.addEventListener(event, callback);
-}
-
-function mouseOutCallback(e) {
-	
-	var image_box = e.target.parentNode;
-	var bottom = image_box.getElementsByClassName("image-bottom")[0];
-	bottom.style.visibility = "hidden";
+	element.addEventListener(event, function () {
+		callback(element)
+	});
 }
