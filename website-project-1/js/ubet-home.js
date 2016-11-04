2392bd02eb736ee9a69375ba14b7bbc3a2f370b4
iStudent <ja.xi.es@hotmail.com>
2016-11-04 11:55:26 -0400

    made featured event boxes look good for mobile

--- website-project-1/js/ubet-home.js
+++ website-project-1/js/ubet-home.js
@@ -7,18 +7,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	var imageHover = document.getElementById('event-div').getElementsByClassName("event-box");
 
-	var num_images = imageHover.length - 1; // remove the last element because it does not have an img
-
 	var menuButton = document.getElementById("mobile-menu-button");
 
-	for (var i = 0; i < num_images; i ++) {
-
-		eventListener(imageHover[i], "mouseover", mouseOverCallback);
-		eventListener(imageHover[i], "mouseleave", mouseOutCallback);
-	} 
+	var featuredBoxes = document.getElementsByClassName("featured-event-box");
 
 	// show image-bottom when hover over image-box
-
+	mouseEventAllElementsIn(imageHover);
+	removeAllImages(featuredBoxes);
 	if (menuButton)
 		eventListener(menuButton, "click", showMenuOnMobile);
 
@@ -52,4 +47,27 @@ function eventListener(element, event, callback) {
 	element.addEventListener(event, function () {
 		callback(element)
 	});
+}
+
+function mouseEventAllElementsIn(arr) {
+
+	var num_images = arr.length - 1;
+
+	for (var i = 0; i < num_images; i ++) {
+
+		eventListener(arr[i], "mouseover", mouseOverCallback);
+		eventListener(arr[i], "mouseleave", mouseOutCallback);
+	}
+}
+
+function removeAllImages(featuredBoxes) {
+
+	for (var i = 0; i < featuredBoxes.length; i ++) {
+
+		if (featuredBoxes[i].getElementsByClassName("featured-event-image")[0].getElementsByTagName("img")[0].src == "") {
+
+			featuredBoxes[i].getElementsByClassName("featured-event-image")[0].style.width = "0px";
+			featuredBoxes[i].getElementsByClassName("featured-event-text")[0].style.width = "100%";
+		}
+	}
 }
