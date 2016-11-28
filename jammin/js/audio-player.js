0b0db6d02548893c9944bf56988d7902ad57b653
JahazielGuzman <ja.xi.es@hotmail.com>
2016-11-28 16:31:23 -0500

    sliding works, selects time on song

--- jammin/js/audio-player.js
+++ jammin/js/audio-player.js
@@ -1,3 +1,4 @@
+// set up variable necessary to operate music player
 var cursorX = -1;
 var playerElem  = null; 
 var drag = null;
@@ -5,9 +6,6 @@ var duration = null;
 
 $(document).ready(function () {
 
-	
-
-	playing = false;
 	songName = "";
 	coverArt = "";
 	artistName = "";
@@ -16,33 +14,46 @@ $(document).ready(function () {
 	currTime = null;
 	currDuration = null;
 	playerElem = document.getElementById("main-player");
+	playerElem.src = "";
 	drag = document.getElementById("seeker");
-	duration = playerElem.duration;
 	
-	pressPlay(playerElem, playing);
+	pressPlay(playerElem);
 	seekAudio();
 });
 
-
-function pressPlay(playerElem, playing) {
+/*
+	take the HTML audio element as playerElem and attach an element to the
+	play button
+*/
+function pressPlay(playerElem) {
 	
 	var butt = document.getElementById("play-button");
+	var song = butt.name;
 	
 	butt.addEventListener("click", function () {
 	
-		pressPlayCallback(playerElem, playing);
+		pressPlayCallback(playerElem, song);
 	});
 }
 
-function pressPlayCallback(playerElem, playing) {
+function pressPlayCallback(playerElem, toPlay) {
 
 	var songName = playerElem.src.split("/");
+	songName = "music/" + songName[songName.length - 1];
+
+	if(songName != toPlay) {
+		 playerElem.src = toPlay;
+		 duration = playerElem.duration;
+	}
+
+	songName = playerElem.src.split("/");
 	songName = songName[songName.length - 1];
-	
+
 	if (!playerElem.paused) {
 		playerElem.pause();
 	}
 	else {
+		
 		playerElem.play();
 	}
 
@@ -51,10 +62,30 @@ function pressPlayCallback(playerElem, playing) {
 
 function moveSeek (cursor) {
 
-	cursorX = cursor.x;
+	var slideBarElem = document.getElementById("slide-bar");
+	var seekerElem = document.getElementById("seeker");
+	var xMin = slideBarElem.getBoundingClientRect().left;
+	var xMax = xMin + slideBarElem.getBoundingClientRect().width;
+
+	if (cursor.x < xMin)
+		cursorX = xMin;
+	else if (cursor.x > xMax)
+		cursorX = xMax;
+	else
+		cursorX = cursor.x;
 
 	drag.style.left = cursorX + "px"; 
 
+	if (playerElem.duration) {
+
+		var timeDistance = cursorX - xMin;
+		var sliderWidth = slideBarElem.getBoundingClientRect().width;
+		var songDuration = playerElem.duration;
+		var timeTakenUp = timeDistance/sliderWidth;
+		var timeToSeek = timeTakenUp * songDuration;
+
+		playerElem.currentTime = timeToSeek;
+	}
 }
 
 function startSeek  () {
@@ -69,7 +100,6 @@ function deregister  () {
 	console.log("deregister")
 	document.removeEventListener("mousemove", moveSeek);
 	document.removeEventListener("mouseup", deregister);
-	cursorX = -1;	
 }
 
 
