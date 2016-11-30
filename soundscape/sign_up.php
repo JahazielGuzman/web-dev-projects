965bb9f5ca22134c800df14e28bee8cad9463da1
iStudent <ja.xi.es@hotmail.com>
2016-11-30 16:08:35 -0500

    Almost done with the sound player, started the sign_up.php

---
+++ soundscape/sign_up.php
@@ -0,0 +1,46 @@
+<?php 
+
+	/*
+	$userData = array(
+		$_POST['username'],
+		$_POST["password"],
+		$_POST["email"],
+		$_POST["date-of-birth"],
+		$_POST["gender"],
+		$_POST["credit-card"],
+		$_POST["expiraton-date"],
+		$_POST["month"],
+		$_POST["year"],
+		$_POST["security-code"],
+		$_POST["zip-code"],
+		$_POST["confirm-password"]
+	);
+
+	// clean each input variable
+	foreach ($userData as &$datum) {
+
+		$datum = mysql_real_escape_string($datum);
+	}
+	*/
+
+	$user = 'root';
+	$password = 'root';
+	$db = 'SoundScape';
+	$host = 'localhost';
+	$port = 8889;
+
+	// initialize the database to insert the users data
+	//$dbConnection = new PDO("mysql:host=127.0.0.1;dbname=". $db, $username, s$password);
+	echo "hello dogg";
+
+	try {
+
+	}
+
+	catch() {
+
+	}
+	// prepare the que ry
+
+	// bind the variables to the query
+?>
